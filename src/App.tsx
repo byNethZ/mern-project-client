6eff61445cd2bc98a0e5ac62835c8227bf6cfdb8
NethZ <bynethz@gmail.com>
2023-10-21 19:05:16 -0500

    adding updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { environments } from "./global";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser: React.FormEventHandler<HTMLFormElement> = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    const response: any = await fetch(environments.urlRegister, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        password: password,
+      })
+
+    })
+
+    const data = await response.json();
+
+    console.log(data);
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={(evt) => registerUser(evt)}>
+        <div>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            name="name"
+            id="name"
+            placeholder="First Name"
+          />
+          <label htmlFor="name">Name</label>
+        </div>
+        <br />
+        <div>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+          <label htmlFor="email">Email</label>
+        </div>
+        <br />
+        <div>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <label htmlFor="password">Password</label>
+        </div>
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 }
---
+++ src/global.ts
@@ -0,0 +1,5 @@
+const urlApi = process.env.REACT_APP_URL_API;
+
+export const environments = {
+    urlRegister: `${urlApi}/register`
+}
